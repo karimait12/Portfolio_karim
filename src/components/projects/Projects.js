2d771cd457e591bf1076956e961d00012c7cacb4
soufiane2001 <67438275+soufiane2001@users.noreply.github.com>
2024-05-02 14:50:06 +0200

    Update Projects.js

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -61,12 +61,12 @@ techs:['#php','#reactnative',"#tailwind","#laravel","#mysql"],type:"mobile"},
         src={x.img} 
         alt="Description of the image"
         width={500}
-        className="h-30 md:h-40"
+        className="h-40 md:h-30"
 
       />
                  <div className="px-6 py-4">
                    <div className="font-bold md:text-xl text-md mb-2">{x.title}</div>
-                        <p className="text-gray-700 text-base  md:text-xl text-md ">
+                        <p className="text-gray-700 text-base  md:text-xl text-sm ">
                                {x.description}
                         </p>
                     </div>
